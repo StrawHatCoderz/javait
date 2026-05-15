8b679c12cab2b5592c28984af824e9203b2b2dc5
Umar Khaji <umarkhaji@Umars-MacBook-Pro.local>
2026-05-16 00:40:49 +0530

    initial commit

---
+++ src/test/java/com/javait/javait/JavaitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javait.javait;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
