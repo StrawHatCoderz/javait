c07b8a858f7347c33b166394747439b208bdbc45
Umar Khaji <umarkhaji@Umars-MacBook-Pro.local>
2026-05-17 16:14:37 +0530

    Ignores cookie files and removes unused imports in controller and utility classes * Adds cookie.txt to .gitignore to prevent sensitive session data from being tracked * Removes the unused GetMapping import in PostController * Deletes unnecessary model and Spring HTTP imports in CookieParser

--- src/main/java/com/javait/utils/CookieParser.java
+++ src/main/java/com/javait/utils/CookieParser.java
@@ -2,12 +2,7 @@ package com.javait.utils;
 
 import com.javait.exceptions.CookieNotFoundException;
 import com.javait.exceptions.TokenNotFoundException;
-import com.javait.models.ApiError;
-import com.javait.models.ApiResponse;
-import com.javait.models.IsLoggedInResponse;
 import jakarta.servlet.http.Cookie;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 public class CookieParser {
   public static String parseJwtToken(Cookie[] cookies) throws CookieNotFoundException, TokenNotFoundException {
